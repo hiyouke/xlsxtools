0175f5ad69f1e82471b81a6bdf30dfa0056ff9af
陈林 <240886393@qq.com>
2019-07-04 23:29:02 +0800

    C#版本优化

--- CSVReader_cs/CSVReader.cs
+++ CSVReader_cs/CSVReader.cs
@@ -1,7 +1,55 @@
+using System;
 using UnityEngine;
 using System.Collections;
 using System.IO;
 using System.Collections.Generic;
+
+public class CSVUtility 
+{	
+	public static int ToInt(string str)
+	{
+		try
+		{
+			return System.Int32.Parse(str);
+		}
+		catch(System.Exception ex)
+		{
+			LogError(ex);
+			return 0;
+		}
+	}
+	
+	public static uint ToUInt(string str)
+	{
+		try
+		{
+			return System.UInt32.Parse(str);
+		}
+		catch(System.Exception ex)
+		{
+			LogError(ex);
+			return 0;
+		}
+	}
+
+	public static float ToFloat(string str)
+	{
+		try
+		{
+			return (float)System.Double.Parse(str);
+		}
+		catch(System.Exception ex)
+		{
+			LogError(ex);
+			
+			return 0;
+		}
+	}
+
+	public static void LogError(object obj)
+	{
+	}
+}
 
 public class RowHelper
 {
