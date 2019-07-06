4180813232199de7f79dfd2e0257fc4afab649e8
陈林 <240886393@qq.com>
2019-07-06 22:57:09 +0800

    csharp版本，代码整理

--- batch_trans.sh
+++ batch_trans.sh
@@ -2,29 +2,27 @@
 runpath=`pwd`
 cd $runpath
 
-echo "remove Config Result"
+echo -e "remove Config Result\n"
 rm -rf ./Result ./Config
 
-#trans
 for f in `cat ./xlsx/list.txt`
 do
     filename=${runpath}/xlsx/$f
-    echo "handle : $filename ...."
+    echo "file:$filename"
     echo 'csharp reader'
     python ${runpath}/cs_reader.py $filename
     echo "java reader"
     python ${runpath}/java_reader.py $filename
 done
 
-echo "csharp manager"
+echo -e "\n------csharp manager------"
 python cs_mgr_gen.py
 
-echo "java manager"
+echo -e "\n------java manager------"
 python java_mgr_gen.py
 
-echo "copy csharp CSVReader"
+echo -e "\n------copy csharp CSVReader------"
 cp ./CSVReader_cs/CSVReader.cs ./Result/cs/CSVReader.cs
-cp ./CSVReader_cs/CSVUtility.cs ./Result/cs/CSVUtility.cs
 
-echo "copy java CSVReader"
+echo -e "\n------copy java CSVReader------"
 cp ./CSVReader_Java/com/xlsxtools/CSVReader.java ./Result/java/com/xlsxtools/CSVReader.java
